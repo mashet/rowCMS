92ae06c57a64cad5360a12874885d805830e0909
joer80 <joer80@gmail.com>
2014-05-21 23:44:19 -0500

    added MysqliDb class

---
+++ database-blank.php
@@ -0,0 +1,8 @@
+<?
+//databasesettings.  Provide as an array so I can pass them easier or short names.
+$dbSettings = array();
+$dbSettings['HN'] = ''; //localhost
+$dbSettings['UN'] = '';
+$dbSettings['PW'] = '';
+$dbSettings['DB'] = '';
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?
+require_once('/cms/classesMysqlidb.php');
+$db = new MysqliDb($dbSettings['HN'], $dbSettings['UN'], $dbSettings['PW'], $dbSettings['DB']);
+
+?>
